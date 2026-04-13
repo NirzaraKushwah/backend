34a6be51280e80eb5fb70d755ce0d4812ed134fb
Nirzara <nirzarakushwah@gmail.com>
2026-04-13 12:41:33 +0530

    commit

--- ApiGateway/ApiGateway/src/main/java/com/lcwd/gateway/ApiGatewayApplication.java
+++ ApiGateway/ApiGateway/src/main/java/com/lcwd/gateway/ApiGatewayApplication.java
@@ -2,12 +2,14 @@ package com.lcwd.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
-public class ConfigServer {
+public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigServer.class, args);
+		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
 }
